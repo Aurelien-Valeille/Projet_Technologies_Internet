bedbfb3ae1fb97724c052b40fd1f576e8fa95327
nico456 <35469185+nico456@users.noreply.github.com>
2018-04-11 17:27:26 -0400

    Add files via upload
    pour pouvoir utiliser le projet:
    
    - installer nodejs 
    - aller à la racine du projet en ligne de commande et installer express
    - aller à la racine du projet en ligne de commande et installer jquery, bodyparser, jsonfile et node-rsa. il faut tapper la commande npm install <nom de la dépendance> --save
    ensuite faire un node app.js et aller sur localhost:3000 
    -----------------------------------------------------------------------------------------------------------------------------------------------------

--- app.js
+++ app.js
@@ -11,156 +11,202 @@ var jsonfile = require('jsonfile');
 var fs = require('fs');
 var NodeRSA = require('node-rsa');
 
-
+//define an absolute path in order to get html and css files
 app.use(express.static('public'));
+
 
 app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist/'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({extended: false}));
+
+//set the view engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 
 
+//user currently logged on the client, usefull to get the username when he sends an email and to get the private key
 var currentUser;
-//sign up
 
-
-app.get('/signup', function(req,res,next){
-    res.sendFile(path.join(__dirname+'/public/HTML/SignUp.html'));
+//get the signup page
+app.get('/signup', function (req, res) {
+    res.sendFile(path.join(__dirname + '/public/HTML/SignUp.html'));
 });
 
-app.post('/gettingdata', function (req, res, next) {
+//post and checking the informations send on the signup form
+app.post('/gettingdata', function (req, res) {
 
-    var username=req.body.username;
-    var password=req.body.password;
+    var username = req.body.username;
+    var password = req.body.password;
+    var publickey = new NodeRSA({b: 512}).exportKey('public') //create new public key
 
-
+    //object will be store in a Json file. There's ine file per user.
     var user = {
         username: username,
         password: password,
-        Publickey: new NodeRSA({b:512}).exportKey('public')
+        Publickey: publickey
     };
 
-    saveUser(user, function(err){
-        if(err){
-            res.status(404).send('User not saved, already exists');
+    //object will be stored in a Json file. Usefull to diplay all the adresses.
+    var carnet = {
+        username: username,
+        publickey: publickey
+    }
+
+    saveUser(user, function (err) {
+        if(err) {
             return;
         }
+    });
+
+    function saveUser(user) {
+        //check if the user already exists
+        if(fs.existsSync('./Database/emails/' + username + '.json')) {
+            res.send('User not saved, already exists');
+            return
+        };
+
+        /*if the user does not exists, write in the carnetAdress and indivudual files. First of all, write in the
+         carnet file. If the file already exists, happen the new datas. If it does not exists, create the file. We have
+         to do so, because the writefile's method erase all the datas before writing new ones*/
+        if(!fs.existsSync('./Database/users/carnetAdress.json')) {
+            fs.writeFileSync('./Database/users/carnetAdress.json', '{"users": [' + JSON.stringify(carnet) + ']}');
+        } else {
+            fs.readFile('./Database/users/carnetAdress.json', 'utf8', function readFileCallback(err, data) {
+                if(err) {
+                    console.log(err);
+                } else {
+                    var obj = JSON.parse(data); //now it an object
+                    obj.users.push(carnet); //add some data
+                    var json = JSON.stringify(obj); //convert it back to json
+                    fs.writeFileSync('./Database/users/carnetAdress.json', json, 'utf8'); // write it back
+                }
+            });
+        }
+        //write in the individual file
+        fs.writeFileSync('./DataBase/users/' + username + '.json', JSON.stringify(user));
+        //modify the permission in a order to allow only read mode
+        fs.chmod('./DataBase/users/' + username + '.json', 4444, function (err) {
+            if(err)
+                console.log(err);
+        });
         res.send('User saved');
-
-    });
-
-
-function saveUser(user, callback){
-    fs.writeFile('./DataBase/users/'+ username + '.json', JSON.stringify(user), callback);
-    fs.chmod('./DataBase/users/'+ username + '.json', 4444, function(err){
-        if(err)
-            console.log(err);
-    });
-}
+    }
 });
 
-//login
-
-app.get('/login', function (req, res, next) {
-    res.sendFile(path.join(__dirname+'/public/HTML/Login.html'));
+//get the login page
+app.get('/login', function (req, res) {
+    res.sendFile(path.join(__dirname + '/public/HTML/Login.html'));
 });
 
+//post the datas from the login form
+app.post('/login', function (req, res) {
 
-app.post('/postlogin', function (req, res, next) {
-
-    fs.open('./DataBase/users/'+ req.body.username + '.json', 'r', (err, fd) => {
-        if (err) {
-            if (err.code === 'ENOENT') {
-                res.send(req.body.username+'does not exists, please sign up')
+    //first of all, check if the user exists. If not, redirect to the signup page
+    fs.open('./DataBase/users/' + req.body.username + '.json', 'r', (err) => {
+        if(err) {
+            if(err.code === 'ENOENT') {
+                res.send(req.body.username + 'does not exists, please sign up')
                 return;
             }
             throw err;
         }
-       fs.readFile('./DataBase/users/'+ req.body.username + '.json', 'utf8', function(err, data){
-           if(err)
-               console.error(err)
-          currentUser=JSON.parse(data);
-           if(currentUser.password === req.body.password) {
-               res.sendFile(path.join(__dirname+'/public/HTML/Courriel.html'));
-           }else{
-               res.redirect('/login');
-           }
-       })
-});
-
-});
-
-//logout
-app.post('/postlogout', function (req, res, next) {
-
-   res.sendFile(path.join(__dirname+'/public/HTML/Login.html'));
-   currentUser.username = null;
-   currentUser.password = null;
-   currentUser.Publickey = null;
-});
-
-app.get('/',function(req,res){
-
-        res.sendFile(path.join(__dirname+'/public/HTML/Login.html'));
+        //read the user's file name and checking the password
+        fs.readFile('./DataBase/users/' + req.body.username + '.json', 'utf8', function (err, data) {
+            if(err)
+                console.error(err)
+            currentUser = JSON.parse(data);
+            if(currentUser.password === req.body.password) {
+                res.redirect('/')
+            } else {
+                res.redirect('/login'); // wrong password, loop on the login page
+            }
+        })
     });
+});
 
-app.get('/EmailListe',function(req,res){
+app.get('/', function (req, res) {
 
-    res.sendFile(path.join(__dirname+'/public/HTML/EmailListe.html'));
+    res.sendFile(path.join(__dirname + '/public/HTML/Courriel.html'));
+});
+
+app.get('/EmailListe', function (req, res) {
+
+    res.sendFile(path.join(__dirname + '/public/HTML/EmailListe.html'));
 
 });
 
-app.get('/ComposeEmail',function(req,res){
+app.get('/ComposeEmail', function (req, res) {
 
-    res.sendFile(path.join(__dirname+'/public/HTML/ComposeEmail.html'));
+    res.sendFile(path.join(__dirname + '/public/HTML/ComposeEmail.html'));
 
 });
+//post datas from the new email form
+app.post('/send', function (req, res) {
 
-app.post('/send', function(req,res) {
-
+    //store the field's data
     var message = req.body.message;
     var destinataire = req.body.destinataire;
     var objet = req.body.objet;
 
+    //create a new empty RSA key
     var key = new NodeRSA();
 
-
-    fs.open('./DataBase/users/' + destinataire + '.json', 'r', (err, fd) => {
+    //check if the recipiant exists in the database
+    fs.open('./DataBase/users/' + destinataire + '.json', 'r', (err) => {
         if(err) {
             if(err.code === 'ENOENT') {
-                res.send(destinataire + ' does not exists')
+                res.send(destinataire + ' does not exists');
                 return;
             }
-            throw err;
         }
     });
 
-        var dest = fs.readFileSync('./DataBase/users/' + destinataire + '.json', 'utf8');
+    //get the public recipiant's public key
+    var dest = fs.readFileSync('./DataBase/users/' + destinataire + '.json', 'utf8');
+    var parsDest = JSON.parse(dest);
+    var keyData = parsDest.Publickey;
+    console.log(keyData);
+    key.importKey(keyData, 'public');
 
-        var parsDest = JSON.parse(dest);
+    //encrypt message
+    var encrypted = key.encrypt(message, 'base64', 'utf8');
 
-        var keyData = parsDest.Publickey;
-        console.log(keyData);
-        key.importKey(keyData, 'public');
+    //object wiil be stored in a Json file
+    var email = {
+        from: currentUser.Publickey,
+        to: parsDest.Publickey,
+        objet: objet,
+        message: encrypted
+    };
 
-            var encrypted = key.encrypt(message, 'base64','utf8');
-
-            var email = {
-                from: currentUser.Publickey,
-                to: parsDest.Publickey,
-                objet: objet,
-                message: encrypted
+    /*if the user does not exists, write in the indivudual email file. If it does not exists, create the file. We have
+     to do so, because the writefile's method erase all the datas before writing new ones*/
+    if(!fs.existsSync('./Database/emails/' + destinataire + '.json')) {
+        fs.writeFileSync('./Database/emails/' + destinataire + '.json', '{"message": [' + JSON.stringify(email) + ']}');
+    } else {
+        fs.readFile('./Database/emails/' + destinataire + '.json', 'utf8', function readFileCallback(err, data) {
+            if(err) {
+                console.log(err);
+            } else {
+                var obj = JSON.parse(data); //now it an object
+                obj.message.push(email); //add some data
+                var json = JSON.stringify(obj); //convert it back to json
+                fs.writeFileSync('./Database/emails/' + destinataire + '.json', json, 'utf8'); // write it back
             }
-    if (!fs.existsSync('./Database/emails/' + destinataire + '.json')) {
-        fs.writeFileSync('./Database/emails/' + destinataire + '.json', JSON.stringify(email));
-    }else{
-                fs.appendFileSync('./Database/emails/' + destinataire + '.json', ','+JSON.stringify(email))
+        });
     }
+    res.send('message envoyé !')
 
 });
-app.get('/CarnetAdresse',function(req,res){
 
-    res.sendFile(path.join(__dirname+'/public/HTML/CarnetAdresse.html'));
+//get the page carnet adresses
+app.get('/CarnetAdresse', function (req, res) {
 
+    var info;
+    jsonfile.readFile('./Database/users/carnetAdress.json', function (err, obj) {
+        info = obj;
+        res.render('userlist', {user: info});
+    });
 });
 
 app.listen(3000);
